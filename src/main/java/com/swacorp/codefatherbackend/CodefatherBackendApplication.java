e932476ce09cd75a7ee6743dd96aa66d5cc30363
jcarter <jeffrey.carter@wnco.com>
2023-10-25 09:41:35 -0500

    initial commit

---
+++ src/main/java/com/swacorp/codefatherbackend/CodefatherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swacorp.codefatherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefatherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodefatherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swacorp/codefatherbackend/CodefatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swacorp.codefatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefatherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
