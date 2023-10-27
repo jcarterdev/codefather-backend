c6bec04e9cacbe07d66a5f28d983bc3f3dd7b7fd
jcarter <jeffrey.carter@wnco.com>
2023-10-26 20:00:59 -0500

    implement forcast by range

--- src/main/java/com/swacorp/codefatherbackend/CodefatherBackendApplication.java
+++ src/main/java/com/swacorp/codefatherbackend/CodefatherBackendApplication.java
@@ -2,6 +2,9 @@ package com.swacorp.codefatherbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CodefatherBackendApplication {
@@ -10,4 +13,13 @@ public class CodefatherBackendApplication {
         SpringApplication.run(CodefatherBackendApplication.class, args);
     }
 
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/lambda")
+                        .uri("https://wg7hulc66yd4fpfn6sdytnr7p40wqjrv.lambda-url.us-east-1.on.aws/"))
+                .build();
+    }
 }
